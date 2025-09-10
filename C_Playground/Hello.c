317f527a48e0f2812e5f22cae66c3df0a5b07b92
root-zero-o <rootzero17@gmail.com>
2025-09-10 21:56:46 +0900

    초기 커밋.

---
+++ C_Playground/Hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+	operator_example();
+	return 0;
+}
---
+++ C_Playground/operator.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int operator_example(void) {
+	int num; // num이라는 이름의 변수 선언
+	num = 10; // 변수 num에 10 저장
+	printf("%d", num); // 변수 num의 값 참조
+}
